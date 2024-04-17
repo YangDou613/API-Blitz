ac177ed3235b17bc039ff1971e3e3904307b7f26
YangDou613 <d860613@gmail.com>
2024-04-17 09:11:19 +0800

    feat: Execute a reset of the automated testing function when the program is restarted.

--- API-Blitz/src/main/java/org/example/apiblitz/ApiBlitzApplication.java
+++ API-Blitz/src/main/java/org/example/apiblitz/ApiBlitzApplication.java
@@ -1,13 +1,24 @@
 package org.example.apiblitz;
 
+import jakarta.annotation.PostConstruct;
+import org.example.apiblitz.service.ResetTestCaseService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiBlitzApplication {
 
+	@Autowired
+	private ResetTestCaseService resetTestCaseService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiBlitzApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		resetTestCaseService.resetTestCase();
 	}
 
 }
