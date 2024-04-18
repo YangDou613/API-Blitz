e54dcbe8879f43d49247a8daf76e6efd352f9933
YangDou613 <d860613@gmail.com>
2024-04-18 18:30:43 +0800

    feat: Modified the ability to update the next test date and time when resetting the test case schedule after a program restart to apply to every time unit.

--- API-Blitz/src/main/java/org/example/apiblitz/service/ResetTestCaseService.java
+++ API-Blitz/src/main/java/org/example/apiblitz/service/ResetTestCaseService.java
@@ -12,6 +12,7 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -38,42 +39,18 @@ public class ResetTestCaseService {
 		for (NextSchedule nextSchedule : nextSchedules) {
 
 			// Get current time
-			LocalDate currentTestDate = LocalDate.now();
-			LocalTime currentTestTime = LocalTime.now();
-			LocalDateTime currentDateTime = LocalDateTime.of(currentTestDate, currentTestTime);
+			LocalDateTime currentDateTime = LocalDateTime.now();
 
 			// Get the next test date and time in the database
-			LocalDate originNextTestDate = nextSchedule.getNextTestDate();
-			LocalTime originNextTestTime = nextSchedule.getNextTestTime();
-			LocalDateTime originNextTestDateTime = LocalDateTime.of(originNextTestDate, originNextTestTime);
+			LocalDateTime nextTestDateTime =
+					LocalDateTime.of(nextSchedule.getNextTestDate(), nextSchedule.getNextTestTime());
 
-			LocalTime originalTestTime;
-
-			while (originNextTestDateTime.isBefore(currentDateTime)) {
-
-				originalTestTime = originNextTestTime;
-
-				switch (nextSchedule.getIntervalsTimeUnit()) {
-					case "Hour":
-						originNextTestTime = originNextTestTime.plusHours(nextSchedule.getIntervalsTimeValue());
-						if (originNextTestTime.isBefore(originalTestTime)) {
-							originNextTestDate = originNextTestDate.plusDays(1);
-						}
-						break;
-					case "Day":
-						originNextTestTime = originNextTestTime.plusHours(nextSchedule.getIntervalsTimeValue() * 24);
-						originNextTestDate = originNextTestDate.plusDays(nextSchedule.getIntervalsTimeValue());
-						break;
-					case "Sec":
-						originNextTestTime = originNextTestTime.plusSeconds(nextSchedule.getIntervalsTimeValue());
-						if (originNextTestTime.isBefore(originalTestTime)) {
-							originNextTestDate = originNextTestDate.plusDays(1);
-						}
-						break;
-				}
-
-				originNextTestDateTime = LocalDateTime.of(originNextTestDate, originNextTestTime);
-
+			if (nextTestDateTime.isBefore(currentDateTime)) {
+				nextTestDateTime = updateNextTestTime(nextSchedule, nextTestDateTime, currentDateTime);
+				testCaseRepository.updateNextTestTime(
+						nextSchedule.getTestCaseId(),
+						nextTestDateTime.toLocalDate(),
+						nextTestDateTime.toLocalTime());
 			}
 
 			// Set test schedule
@@ -84,31 +61,13 @@ public class ResetTestCaseService {
 					autoTestService.autoTest(nextSchedule.getTestCaseId());
 					log.info(timestamp + " : testCaseId <" + nextSchedule.getTestCaseId() + "> Finish testing!");
 
-					LocalDate nextTestDate = timestamp.toLocalDateTime().toLocalDate();
-					LocalTime nextTestTime = timestamp.toLocalDateTime().toLocalTime();
+					// Calculate next test date and time
+					LocalDateTime updateNextTestDateTime = calculateNextTestTime(nextSchedule, timestamp);
 
-					LocalTime originalTime = nextTestTime;
-
-					switch(nextSchedule.getIntervalsTimeUnit()) {
-						case "Hour":
-							nextTestTime = nextTestTime.plusHours(nextSchedule.getIntervalsTimeValue());
-							if (nextTestTime.isBefore(originalTime)) {
-								nextTestDate = nextTestDate.plusDays(1);
-							}
-							break;
-						case "Day":
-							nextTestTime = nextTestTime.plusHours(nextSchedule.getIntervalsTimeValue() * 24);
-							nextTestDate = nextTestDate.plusDays(nextSchedule.getIntervalsTimeValue());
-							break;
-						case "Sec":
-							nextTestTime = nextTestTime.plusSeconds(nextSchedule.getIntervalsTimeValue());
-							if (nextTestTime.isBefore(originalTime)) {
-								nextTestDate = nextTestDate.plusDays(1);
-							}
-							break;
-					}
-
-					testCaseRepository.updateNextTestTime(nextSchedule.getTestCaseId(), nextTestDate, nextTestTime);
+					testCaseRepository.updateNextTestTime(
+							nextSchedule.getTestCaseId(),
+							updateNextTestDateTime.toLocalDate(),
+							updateNextTestDateTime.toLocalTime());
 
 				} catch (IOException e) {
 					throw new RuntimeException(e);
@@ -118,26 +77,97 @@ public class ResetTestCaseService {
 			// Get intervals time unit
 			String intervalsTimeUnit = nextSchedule.getIntervalsTimeUnit();
 
+			// Get intervals time value
+			Integer intervalsTimeValue = nextSchedule.getIntervalsTimeValue();
+
 			TimeUnit timeUnit = null;
+			long initialDelay = 0;
 
 			switch (intervalsTimeUnit) {
 				case "Hour":
 					timeUnit = TimeUnit.HOURS;
+					initialDelay = ChronoUnit.HOURS.between(currentDateTime, nextTestDateTime);
 					break;
 				case "Day":
 					timeUnit = TimeUnit.DAYS;
+					initialDelay = ChronoUnit.DAYS.between(currentDateTime, nextTestDateTime);
 					break;
 				case "Sec":
 					timeUnit = TimeUnit.SECONDS;
+					initialDelay = ChronoUnit.SECONDS.between(currentDateTime, nextTestDateTime);
 					break;
 			}
 
-			// Get intervals time value
-			Integer intervalsTimeValue = nextSchedule.getIntervalsTimeValue();
-
-			executor.scheduleAtFixedRate(test, 0, intervalsTimeValue, timeUnit);
+			executor.scheduleAtFixedRate(test, initialDelay, intervalsTimeValue, timeUnit);
 
 			testCaseRepository.updateResetStatusByTestCaseId(nextSchedule.getTestCaseId());
 		}
+	}
+
+	public LocalDateTime updateNextTestTime(NextSchedule nextSchedule,
+	                                        LocalDateTime nextTestDateTime,
+	                                        LocalDateTime currentDateTime) {
+
+		LocalTime originalTestTime;
+		LocalDate nextTestDate = nextTestDateTime.toLocalDate();
+		LocalTime nextTestTime = nextTestDateTime.toLocalTime();
+		LocalDateTime updateNextTestTime = nextTestDateTime;
+
+		while (updateNextTestTime.isBefore(currentDateTime)) {
+
+			originalTestTime = nextTestTime;
+
+			switch (nextSchedule.getIntervalsTimeUnit()) {
+				case "Hour":
+					nextTestTime = nextTestTime.plusHours(nextSchedule.getIntervalsTimeValue());
+					if (nextTestTime.isBefore(originalTestTime)) {
+						nextTestDate = nextTestDate.plusDays(1);
+					}
+					break;
+				case "Day":
+					nextTestTime = nextTestTime.plusHours(nextSchedule.getIntervalsTimeValue() * 24);
+					nextTestDate = nextTestDate.plusDays(nextSchedule.getIntervalsTimeValue());
+					break;
+				case "Sec":
+					nextTestTime = nextTestTime.plusSeconds(nextSchedule.getIntervalsTimeValue());
+					if (nextTestTime.isBefore(originalTestTime)) {
+						nextTestDate = nextTestDate.plusDays(1);
+					}
+					break;
+			}
+
+			updateNextTestTime = LocalDateTime.of(nextTestDate, nextTestTime);
+		}
+
+		return updateNextTestTime;
+	}
+
+	public LocalDateTime calculateNextTestTime(NextSchedule nextSchedule, Timestamp timestamp) {
+
+		LocalDate nextTestDate = timestamp.toLocalDateTime().toLocalDate();
+		LocalTime nextTestTime = timestamp.toLocalDateTime().toLocalTime();
+
+		LocalTime originalTime = nextTestTime;
+
+		switch(nextSchedule.getIntervalsTimeUnit()) {
+			case "Hour":
+				nextTestTime = nextTestTime.plusHours(nextSchedule.getIntervalsTimeValue());
+				if (nextTestTime.isBefore(originalTime)) {
+					nextTestDate = nextTestDate.plusDays(1);
+				}
+				break;
+			case "Day":
+				nextTestTime = nextTestTime.plusHours(nextSchedule.getIntervalsTimeValue() * 24);
+				nextTestDate = nextTestDate.plusDays(nextSchedule.getIntervalsTimeValue());
+				break;
+			case "Sec":
+				nextTestTime = nextTestTime.plusSeconds(nextSchedule.getIntervalsTimeValue());
+				if (nextTestTime.isBefore(originalTime)) {
+					nextTestDate = nextTestDate.plusDays(1);
+				}
+				break;
+		}
+
+		return LocalDateTime.of(nextTestDate, nextTestTime);
 	}
 }
