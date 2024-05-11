65b7238736715ff98fe70d6d99814ad97178294f
YangDou613 <d860613@gmail.com>
2024-05-11 09:55:41 +0800

    feat: Modify user sign in and sign up UI.

--- API-Blitz/src/main/resources/static/signUpIn.js
+++ API-Blitz/src/main/resources/static/signUpIn.js
@@ -1,3 +1,35 @@
+const welcomeMessage = document.getElementById("welcome-message");
+const line1 = document.getElementById("line1");
+const line2 = document.getElementById("line2");
+const line3 = document.getElementById("line3");
+
+const text1 = "Welcome!";
+// const text2 = "Are you looking for a flexible and reliable way to test your API?";
+// const text3 = "You've come to the right place! We provide one-stop API testing related services, allowing you to easily test and monitor your applications.";
+
+let index1 = 0;
+let index2 = 0;
+let index3 = 0;
+
+function typeWriter() {
+    if (index1 < text1.length) {
+        line1.innerHTML += text1.charAt(index1);
+        index1++;
+        setTimeout(typeWriter, 100);
+    }
+    // else if (index2 < text2.length) {
+    //     line2.innerHTML += text2.charAt(index2);
+    //     index2++;
+    //     setTimeout(typeWriter, 10);
+    // } else if (index3 < text3.length) {
+    //     line3.innerHTML += text3.charAt(index3);
+    //     index3++;
+    //     setTimeout(typeWriter, 10);
+    // }
+}
+
+window.onload = typeWriter;
+
 document.getElementById('SignUpForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
